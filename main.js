dd620f0d62e44a8e3f7c6acb1c6efd55c56f614d
Bradley Hnatow <67875274+hnatowbk@users.noreply.github.com>
2021-06-10 12:48:26 -0400

    Update main.js

--- main.js
+++ main.js
@@ -13,3 +13,37 @@ class Block{
         return SHA256(this.index + this.previousHash + this.timestamp +JSON.stringify(this,this.data)).toString();
     }
 }
+
+class Blockchain{
+    constructor(){
+        this.chain = [];
+    }
+
+    createGenesisBlock(){
+        return new Block(0, "05/09/2021", "JesusOwnedScrantonCoin", "0")
+    }
+
+    getLatestBlock(){
+        return this.chain[this.chain.length-1];
+    }
+
+    addBlock(newBlock){
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock)
+    }
+
+    isChainValid(){
+        for(let i = 1; i < this.chain.length; i++){
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i-1];
+            if(currentBlock.hast != currentBlock.calculateHash()){
+                return false;
+            }
+            if(currentBlock.previousHash != previousBlock.hash){
+                return false;
+            }
+        }
+        return true;
+    }
+}
