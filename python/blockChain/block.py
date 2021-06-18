9b5ee4d1385f28fd3ead1a7f9b236271b328b3fa
hnatowbk <67875274+hnatowbk@users.noreply.github.com>
2021-06-18 13:40:46 -0400

    block 0.00.01

--- python/blockChain/block.py
+++ python/blockChain/block.py
@@ -1,16 +1,29 @@
 import datetime
 import hashlib
 
+"""
+
+Author: Bradley K. Hnatow
+
+"""
+
 
 class block():
 
-    def __init__(self, prevHash, blockIndex, transaction):
+    def __init__(self, prevHash, blockIndex, data):
         self.timeOfCreation = block.calculateTime()
         self.prevHash = prevHash
         self.blockIndex = blockIndex
-        self.transaction = transaction
+        self.data = data
         self.blockHash = block.calculateHash(self)
         self.proofOfWork = None
+
+# calculateTime() 
+# Calculates the time that the newly created block was created
+# Sets the date of the block in a (Year, Day Of Week, Month, 
+#                                  Date, Hour, Minute, Second) Format
+#
+# @returns setDate
 
     @staticmethod
     def calculateTime():
@@ -18,20 +31,29 @@ class block():
         setDate = date.strftime("%Y %a %b %d %H:%M:%S")
         return setDate
 
-    def returnDate(self):
-        return self.timeOfCreation
+# calculateHash(self)
+# Generates a hash for the newly created block by using 
+# variables like timeOfCreation, blockIndex, and the data.
+# Hash is generated using sha256
+#
+# @returns void
 
     def calculateHash(self):
         hashCode = hashlib.sha256(
-            (str(self.timeOfCreation) + str(self.blockIndex) + self.transaction).encode('utf-8')).hexdigest()
+            (str(self.timeOfCreation) + str(self.blockIndex) + self.data).encode('utf-8')).hexdigest()
         self.blockHash = hashCode
         return
+
+# Simple return Methods used for testing
 
     def returnHash(self):
         return self.blockHash
 
-    def returnTrans(self):
-        return self.transaction
+    def returnData(self):
+        return self.data
+
+    def returnDate(self):
+        return self.timeOfCreation
 
 
 testBlock = block("62f8d31406ce34366a8d5112fba87826c713ec2fb43419ed5ac9ccc45cd99780",
