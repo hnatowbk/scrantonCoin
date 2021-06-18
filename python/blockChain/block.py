b8aeb3a1f6d0d28e2b6b1a92f3006878082cbb96
hnatowbk <67875274+hnatowbk@users.noreply.github.com>
2021-06-18 13:00:21 -0400

    block Init

--- python/blockChain/block.py
+++ python/blockChain/block.py
@@ -1,24 +1,38 @@
 import datetime
-from hashlib import sha256
+import hashlib
+
 
 class block():
 
-    def __init__ (self, prevHash, blockIndex):
+    def __init__(self, prevHash, blockIndex, data):
         self.prevHash = prevHash
-        self.timeOfCreation = calculateTime()
+        self.timeOfCreation = block.calculateTime()
         self.blockIndex = blockIndex
-        self.blockHash = blockHash
+        self.blockHash = block.calculateHash(self)
         self.data = data
-        self.proofOfWork = proofOfWork
+        self.proofOfWork = None
 
-    def calculateTime(self):
+    @staticmethod
+    def calculateTime():
         date = datetime.datetime.now()
-        hashDate = date.strftime("%Y %a %b %d %H:%M:%S")
-        return hashDate
+        setDate = date.strftime("%Y %a %b %d %H:%M:%S")
+        return setDate
 
     def returnDate(self):
-        return self.dateOfCreation
+        return self.timeOfCreation
+
+    def calculateHash(self):
+        hashCode = hashlib.sha256(
+            str(self.timeOfCreation).encode('utf-8')).hexdigest()
+        self.blockHash = hashCode
+        return
+
+    def returnHash(self):
+        return self.blockHash
 
 
-#dateOfCreation = str(calculateTime())
-#blockIndex = hashlib.sha256(str(dateOfCreation).encode('utf-8'))
+testBlock = block(0, 1)
+print(testBlock.returnDate())
+testBlock.calculateHash()
+print("")
+print(testBlock.returnHash())
