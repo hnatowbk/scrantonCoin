cb7b8001f9769c4ed440d4ec3071cc80e5637c1e
hnatowbk <67875274+hnatowbk@users.noreply.github.com>
2021-06-20 15:11:03 -0400

    Refactor Block

--- block.py
+++ block.py
@@ -1,6 +1,7 @@
 import datetime
 import hashlib
 import array
+import json
 
 """
 Title: Python Block Chain
@@ -15,56 +16,49 @@ to be used and updated for more complex projects.
 
 class block():
 
-    def __init__(self, prevHash, blockIndex, data):
+    def __init__(self, prevHash, dataArray, proof, nonce=0):
         self.timeOfCreation = block.calculateTime()
         self.prevHash = prevHash
-        self.blockIndex = blockIndex
-        self.data = data
-        self.blockHash = ""
-        block.calculateHash(self)
-        self.proofOfWork = None
+        self.dataArray = dataArray
+        self.hash = block.calculateHash(self)
+        self.nonce = nonce
 
     # calculateTime()
-    #
-    # Calculates the time that the newly created block was created
-    # Sets the date of the block in a (Year, Day Of Week, Month, Date,
-    #                                  Hour, Minute, Second) format.
-    #
-    # @returns setDate
+    #      Calculates the time that the newly created block was created
+    #      Sets the date of the block in a (Year, Day Of Week, Month, Date,
+    #                                       Hour, Minute, Second) format.
+    # @returns String
 
     @staticmethod
     def calculateTime():
         date = datetime.datetime.now()
-        setDate = date.strftime("%Y %a %b %d %H:%M:%S")
-        return setDate
+        formalDate = date.strftime("%Y %a %b %d %H:%M:%S")
+        return formalDate
 
     # calculateHash(self)
-    #
-    # Generates a hash for the newly created block by using
-    # variables like timeOfCreation, blockIndex, and the data.
-    # Hash is generated using sha256
-    #
+    #       Generates a hash for the newly created block by using
+    #       variables like timeOfCreation, and the prevHash.
+    #       Hash is generated using sha256
     # @returns void
 
     def calculateHash(self):
         hashCode = hashlib.sha256(
-            (str(self.timeOfCreation) + str(self.blockIndex) + self.data).encode('utf-8')).hexdigest()
-        self.blockHash = hashCode
+            (str(self.timeOfCreation) + self.prevHash).encode('utf-8')).hexdigest()
+        self.hash = hashCode
 
 
 class blockChain():
 
     def __init__(self):
+        self.unconfirmed = []
         self.chain = []
-        genesis = block("0000", 0, "Genesis Block")
+        genesis = block("0000", [], )  #(self, prevHash, data, proof)
         self.chain.append(genesis)
         self.valid = True
 
     # printBlockChain(self)
-    #
-    # Prints the current block chain providing each
-    # block's Data, Time of creation, Hash, & PrevHash
-    #
+    #      Prints the current block chain providing each
+    #      block's Data, Time of creation, Hash, & PrevHash
     # @reutns void
 
     def printBlockChain(self):
@@ -77,30 +71,37 @@ class blockChain():
             )
 
     # getLatestHash(self)
-    # 
-    # Provides the latest hash of blocks in the blockChain
-    #
-    # @returns str(self.chain[len(self.chain)-1].blockHash)
-
+    #      Provides the latest hash of blocks in the blockChain
+    # @returns String
+   
+    @property
     def getLatestHash(self):
         return str(self.chain[len(self.chain)-1].blockHash)
 
+    # getLatestBlock(self)
+    #      Returns the most recently added block on the chain
+    # @returns <block>
+
+    @property
+    def getLatestBlock(self):
+        return self.chain[-1]
+
+    
+    def addNewUnconfirmed(self,data):
+        self.unconfirmed.append(data)
+
     # addBlock(self, data)
-    #
-    # Adds a new block to the chain array
-    #
+    #      Adds a new block to the chain array
     # @reutrns void
 
-    def addBlock(self, data):
+    def addBlock(self, data, proof):
         newBlock = block(blockChain.getLatestHash(self), len(self.chain), data)
         self.chain.append(newBlock)
         self.testChainIntegrity()
 
     # testChainIntegrity(self)
-    #
-    # Checks the current self.chain[x].hash and compares it to 
-    # next block in the chain's prevHash
-    #
+    #      Checks the current self.chain[x].hash and compares it to 
+    #      next block in the chain's prevHash
     # @returns void
 
     def testChainIntegrity(self):
@@ -119,7 +120,7 @@ class blockChain():
             
 
 testBlockChain = blockChain()
-'''
+
 testBlockChain.printBlockChain()
 testBlockChain.addBlock("Second Block")
 testBlockChain.printBlockChain()
@@ -128,4 +129,3 @@ testBlockChain.printBlockChain()
 testBlockChain.addBlock("Forth Block")
 testBlockChain.printBlockChain()
 testBlockChain.testChainIntegrity()
-'''
